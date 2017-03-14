3d00b2408bbe54ddb45feb0d4a2465f97811f195
Dan Ward <daniel.ward@forrent.com>
2017-03-14 18:16:25 -0400

    Build basic up / down, random placement of fireworks

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
+var fireworks = [];
+var gravity;
+
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
+    gravity = createVector(0, 0.2);
+    stroke(255);
+    strokeWeight(4);
 }
 
 function draw() {
-
+    background(51);
+    fireworks.push(new Firework());
+    for (var i = 0; i < fireworks.length; i++) {
+        fireworks[i].update();
+        fireworks[i].show();
+    }
 }
 
 window.onresize = function() {
---
+++ particle.js
@@ -0,0 +1,19 @@
+function Particle(x, y) {
+    this.position = createVector(x, y);
+    this.velocity = createVector(0, -10);
+    this.acceleration = createVector(0, 0);
+
+    this.applyForce = function(force) {
+        this.acceleration.add(force);
+    }
+
+    this.update = function() {
+        this.velocity.add(this.acceleration);
+        this.position.add(this.velocity);
+        this.acceleration.mult(0);
+    }
+
+    this.show = function() {
+        point(this.position.x, this.position.y);
+    }
+}
