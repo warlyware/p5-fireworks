af2a33576756582ae776866e5f6e6b101c68ec33
Dan Ward <daniel.ward@forrent.com>
2017-03-15 17:54:41 -0400

    Make firework particles fade out after explosion

--- particle.js
+++ particle.js
@@ -1,6 +1,8 @@
 function Particle(x, y, firework) {
     this.position = createVector(x, y);
     this.firework = firework;
+    this.lifespan = 255;
+
     if (this.firework) {
         this.velocity = createVector(0, random(-19, -5));
     } else {
@@ -14,12 +16,23 @@ function Particle(x, y, firework) {
     }
 
     this.update = function() {
+        if (!this.firework) {
+            this.velocity.mult(0.85);
+            this.lifespan -= 4;
+        }
         this.velocity.add(this.acceleration);
         this.position.add(this.velocity);
         this.acceleration.mult(0);
     }
 
     this.show = function() {
+        if (!this.firework) {
+            strokeWeight(2);
+            stroke(255, this.lifespan);
+        } else {
+            strokeWeight(4);
+            stroke(255);
+        }
         point(this.position.x, this.position.y);
     }
 }
